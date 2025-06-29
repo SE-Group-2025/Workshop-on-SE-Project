1ab055fdaa10321156f52943b773d0b7b68ffa17
gil eden <gilede@post.bgu.ac.il>
2025-06-29 11:51:10 +0300

    fix

--- src/main/java/com/SEGroup/Domain/Store/Store.java
+++ src/main/java/com/SEGroup/Domain/Store/Store.java
@@ -22,7 +22,7 @@ public class Store {
     @Column(name = "name", unique = true, nullable = false)
     private String name;
 
-    @Column(name = "email", unique = true, nullable = false)
+    @Column(name = "email")
     private String founderEmail;
 
     @Column(name = "is_active")
